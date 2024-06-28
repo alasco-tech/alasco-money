8d25894c03fb4abc0291694863522c69c16a0a71
Lorenzo Peña <lorenzo.pena@alasco.de>
2024-06-28 16:37:33 +0200

    Switched to declarative module definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
+use pyo3::prelude::*;
+
 mod decimals;
 mod money;
 mod money_vat;
 mod money_vat_ratio;
 
-use env_logger;
-use pyo3::prelude::*;
-
-use crate::money::sum_;
-use crate::money::Money;
-use crate::money_vat::MoneyWithVAT;
-use crate::money_vat_ratio::MoneyWithVATRatio;
-
 #[pymodule]
-fn alasco_money(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    env_logger::init(); // Initialize logging
-    m.add_class::<Money>()?;
-    m.add_class::<MoneyWithVAT>()?;
-    m.add_class::<MoneyWithVATRatio>()?;
-    m.add_function(wrap_pyfunction!(sum_, m)?)?;
-    Ok(())
+mod alasco_money {
+    use super::*;
+    use env_logger;
+
+    #[pymodule_export]
+    use crate::money::Money;
+
+    #[pymodule_export]
+    use crate::money_vat::MoneyWithVAT;
+
+    #[pymodule_export]
+    use crate::money_vat_ratio::MoneyWithVATRatio;
+
+    #[pymodule_export]
+    use crate::money::sum_;
+
+    #[pymodule_init]
+    fn init(_m: &Bound<'_, PyModule>) -> PyResult<()> {
+        env_logger::init(); // Initialize logging
+        Ok(())
+    }
 }
